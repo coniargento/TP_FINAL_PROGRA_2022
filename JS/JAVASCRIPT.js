524d8bf155b01f0025065cca41debb3fd4c6b468
coniargento <argentoconstanza@gmail.com>
2022-11-06 16:06:18 -0300

    cambios

--- JS/JAVASCRIPT.js
+++ JS/JAVASCRIPT.js
@@ -1,4 +1,12 @@
-document.onkeydown = function (event){
+/*document.onkeydown = function (event){
 
     document.write(event);
+} */
+
+var cambio = function(){
+    var nom = prompt("Ingrese su nombre");
+    document.getElementById("nombre").innerHTML = "Bienvenido " + nom;
 }
+
+document.getElementById("jugar").onclick = cambio;
+
