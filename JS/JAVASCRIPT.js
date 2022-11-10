1dfd0ad00ac097f71d0005c0109391dcf684f95c
coniargento <argentoconstanza@gmail.com>
2022-11-10 09:27:02 -0300

    arrays

--- JS/JAVASCRIPT.js
+++ JS/JAVASCRIPT.js
@@ -1,15 +1,15 @@
 /*document.onkeydown = function (event){
 
     document.write(event);
-}
+}*/
 
 var cambio = function(){
-    var nom = prompt("Ingrese su nombre");
+    var nom = document.getElementById("pedido").value;
     document.getElementById("nombre").innerHTML = "Bienvenido " + nom;
 }
 
 document.getElementById("jugar").onclick = cambio;
-*/
 
-var palabras = [Heurística, Prototipo, Research, Patrones, Funcionalidad, Interfaz, Benchmark, Encuestas, Arquetipos, Interacción, Contenido, Usuario, Experiencia, Colores, Hipótesis, Tipografía, Grilla, Metodología, Testeos, Ética];
+
+var palabras = ["Heurística", "Prototipo", "Research", "Patrones", "Funcionalidad", "Interfaz", "Benchmark", "Encuestas", "Arquetipos", "Interacción", "Contenido", "Usuario", "Experiencia", "Colores", "Hipótesis", "Tipografía", "Grilla", "Metodología", "Testeos", "Ética"];
 
