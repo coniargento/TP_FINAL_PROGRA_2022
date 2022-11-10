2455015697b5ac04f30db3de5f6ac09c708ac80d
coniargento <argentoconstanza@gmail.com>
2022-11-09 21:57:18 -0300

    array palabras

--- JS/JAVASCRIPT.js
+++ JS/JAVASCRIPT.js
@@ -1,7 +1,7 @@
 /*document.onkeydown = function (event){
 
     document.write(event);
-} */
+}
 
 var cambio = function(){
     var nom = prompt("Ingrese su nombre");
@@ -9,4 +9,7 @@ var cambio = function(){
 }
 
 document.getElementById("jugar").onclick = cambio;
+*/
+
+var palabras = [Heurística, Prototipo, Research, Patrones, Funcionalidad, Interfaz, Benchmark, Encuestas, Arquetipos, Interacción, Contenido, Usuario, Experiencia, Colores, Hipótesis, Tipografía, Grilla, Metodología, Testeos, Ética];
 
