c59ade6e5b24ce77858683f4e45fc3cb57f5b7b8
coniargento <argentoconstanza@gmail.com>
2022-11-10 10:13:11 -0300

    cambios

--- JS/JAVASCRIPT.js
+++ JS/JAVASCRIPT.js
@@ -4,39 +4,28 @@
 }*/
 
 var cambio = function(){
-
     var nom = document.getElementById("pedido").value;
-<<<<<<< HEAD
-
-        document.getElementById("nombre").innerHTML = "Bienvenido " + nom;
-=======
     document.getElementById("nombre").innerHTML = "¿Estas listx para jugar " + nom + "?";
->>>>>>> 602b3b497fba2be72ef81360db27af475366072f
 }
 
 document.getElementById("jugar").onclick = cambio;
 
 //DECLARAMOS LA VARIABLE DE TECLADO
 var teclado = "";
-
 //ARRAY LETRAS PARA HACER EL TECLADO
 var letras = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //FOR PARA QUE RECORRA ESE ARRAY Y OBTENGAMOS UN BOTON CON CADA LETRA
-
 for(i=0; i<letras.length; i++){
-
     teclado += "<button class= 'boton' onclick= 'mostrarletra(\""+letras[i]+"\")'"
-
         letras[i]+"</button>";
 }
 
 document.getElementById("botones").innerHTML = teclado;
 
-    function mostrarletra(letras){
-
-        alert(letras);
-    }
+function mostrarletra(letras){
+    alert(letras);
+}
 
 
 var palabras = ["Heurística", "Prototipo", "Research", "Patrones", "Funcionalidad", "Interfaz", "Benchmark", "Encuestas", "Arquetipos", "Interacción", "Contenido", "Usuario", "Experiencia", "Colores", "Hipótesis", "Tipografía", "Grilla", "Metodología", "Testeos", "Ética"];
